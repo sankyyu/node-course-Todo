35ed666f8f7eb5223ee5033d19bec0e04d8736c6
sankyyu <syu10@stevens.edu>
2017-05-25 15:24:59 -0400

    add post /todos and refactor mongoose

---
+++ server/server.js
@@ -0,0 +1,32 @@
+var express=require('express');
+var bodyParser=require('body-parser');
+
+var {mongoose}=require('./db/mongoose.js');
+var {Todo}=require('./models/todo.js');
+var {User}=require('./models/user.js');
+
+var app=express();
+// app.get('/',(req,res)=>{
+//   res.send('hello Express');
+// });
+
+app.use(bodyParser.json());
+
+
+app.post('/todos',(req,res)=>{
+  console.log(req);
+  var todo=new Todo({
+    text:req.body.text
+  });
+
+  todo.save().then((doc)=>{
+    res.send(doc);
+  },(err)=>{
+    res.status(400).send(err);
+  });
+
+});
+
+app.listen(3000,()=>{
+  console.log('Started on port 3000');
+});
