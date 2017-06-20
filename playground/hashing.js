01e0dc9ed3f58c177c6ac69275c19315a510815a
sankyyu <syu10@stevens.edu>
2017-06-19 22:44:41 -0400

    Add POST /users routr and token method

---
+++ playground/hashing.js
@@ -0,0 +1,19 @@
+const {SHA256} =require('crypto-js');
+const jwt=require()
+// var message='I am user numer3';
+// var hash=SHA256(message).toString();
+//
+// console.log('Message: '+message);
+// console.log('Hash: '+hash);
+// 
+// var data={
+//   id:4
+// }
+//
+// var token={
+//   data,
+//   hash:SHA256(JSON.stringfy(data)+'somesecret').toString()
+// }
+//
+//
+//
--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ var {Todo}=require('./models/todo.js');
 var {User}=require('./models/user.js');
 
 var app=express();
-const port=process.env.PORT || 3014;
+const port=process.env.PORT || 3001;
 // app.get('/',(req,res)=>{
 //   res.send('hello Express');
 // });
@@ -101,6 +101,18 @@ app.patch('/todos/:id',(req,res)=>{
   })
 });
 
+app.post('/users',(req,res)=>{
+  var body=_.pick(req.body,['email','password']);
+  var user=new User(body);
+
+  user.save().then(()=>{
+    return user.generateAuthToken();
+  }).then((token)=>{
+    res.header('x-auth',token).send(user);
+  }).catch((e)=>{
+    res.status(400).send(e);
+  })
+});
 
 app.listen(port,()=>{
   console.log('Started on port '+port);
